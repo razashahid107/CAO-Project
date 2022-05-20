7d0487410512528b6df48db3a7a202b06d631f49
razashahid107 <razashahid107@gmail.com>
2022-05-20 10:12:59 +0500

    dht11 works but not the bmp180 :(

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,19 @@
+// bigger leg is anode 
+
 void setup()
 {
-pinMode(32, OUTPUT); // Set GPIO22 as digital output pin
+pinMode(4, OUTPUT); // Set GPIO22 as digital output pin
+//pinMode(5, OUTPUT);
 }
 
 void loop() 
 {
-digitalWrite(32, HIGH); // Set GPIO22 active high
+digitalWrite(4, HIGH); // Set GPIO22 active high
+//digitalWrite(5, HIGH); // Set GPIO22 active high
+
 delay(1000);  // delay of one second
-digitalWrite(32, LOW); // Set GPIO22 active low
+digitalWrite(4, LOW); // Set GPIO22 active low
+//digitalWrite(5, LOW); // Set GPIO22 active low
+
 delay(1000); // delay of one second
 }
