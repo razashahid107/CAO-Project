6247d3513c0bdfb4892f762c420605a7921f82f7
razashahid107 <razashahid107@gmail.com>
2022-05-26 13:00:37 +0500

    Added code with Server

--- main/DHTtester/DHTtester.ino
+++ main/DHTtester/DHTtester.ino
@@ -1,3 +1,65 @@
+///*
+// * Rui Santos 
+// * Complete Project Details https://randomnerdtutorials.com
+// */
+//
+//// Load required libraries
+//#include <WiFi.h>
+//#include "SD.h"
+//#include "DHT.h"
+//#include <Wire.h>
+//#include <Adafruit_BMP085.h>
+//
+//// Replace with your network credentials
+//const char* ssid     = "gg";
+//const char* password = "hello123";
+//
+//// uncomment one of the lines below for whatever DHT sensor type you're using
+//#define DHTTYPE DHT11   // DHT 11
+////#define DHTTYPE DHT21   // DHT 21 (AM2301)
+////#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//
+//// GPIO the DHT is connected to
+//const int DHTPin = 15;
+////intialize DHT sensor
+//DHT dht(DHTPin, DHTTYPE);
+//
+//// create a bmp object
+//Adafruit_BMP085 bmp;
+//
+//// Web page file stored on the SD card
+//File webFile; 
+//
+//// Set potentiometer GPIO
+//const int potPin = 32;
+//
+//// IMPORTANT: At the moment, GPIO 4 doesn't work as an ADC when using the Wi-Fi library
+//// This is a limitation of this shield, but you can use another GPIO to get the LDR readings
+//const int LDRPin = 4;
+//
+//// variables to store temperature and humidity
+//float tempC;
+//float tempF;
+//float humi;
+//
+//// Variable to store the HTTP request
+//String header;
+//
+//// Set web server port number to 80
+//WiFiServer server(80);
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
 
@@ -35,6 +97,27 @@ void setup() {
   Serial.println(F("DHTxx test!"));
 
   dht.begin();
+
+//   if (!bmp.begin()){
+//    Serial.println("Could not find BMP180 or BMP085 sensor");
+//    while (1) {}
+//  }
+//
+//  Serial.print("Connecting to ");
+//  Serial.println(ssid);
+//  WiFi.begin(ssid, password);
+//
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(500);
+//    Serial.print(".");
+//  }
+//
+//  // Print local IP address and start web server
+//  Serial.println("");
+//  Serial.println("WiFi connected.");
+//  Serial.println("IP address: ");
+//  Serial.println(WiFi.localIP());
+//  server.begin();
 }
 
 void loop() {
--- main/main.ino
+++ main/main.ino
@@ -1,19 +1,237 @@
-// bigger leg is anode 
+#include "DHT.h"
+#include "AsyncTCP.h"
+#include "ESPAsyncWebServer.h"
+#include "WiFi.h"
 
-void setup()
-{
-pinMode(4, OUTPUT); // Set GPIO22 as digital output pin
-//pinMode(5, OUTPUT);
+#define LED 2
+#define DHTPIN 4
+DHT dht(DHTPIN, DHT11);
+float heat;
+float humidity;
+float temperature;
+
+const char* SSID = "Ali";
+const char* PASS = "554151955";
+
+AsyncWebServer server(80);
+
+//***************************************************************************************************************************************************************
+
+String readTemp() {
+  temperature = dht.readTemperature();
+  while (isnan(temperature)) {
+    temperature = dht.readTemperature();
+  }  
+  return String(temperature);
 }
 
-void loop() 
-{
-digitalWrite(4, HIGH); // Set GPIO22 active high
-//digitalWrite(5, HIGH); // Set GPIO22 active high
+String readHumidity() {
+ humidity = dht.readHumidity();
+ while (isnan(humidity)) {
+    humidity = dht.readHumidity();
+  }  
+ return String(humidity);
+}
 
-delay(1000);  // delay of one second
-digitalWrite(4, LOW); // Set GPIO22 active low
-//digitalWrite(5, LOW); // Set GPIO22 active low
+String readHeat() {
+  heat = dht.computeHeatIndex(temperature, humidity, false);
+  return String(heat);
+}
 
-delay(1000); // delay of one second
+
+
+
+//***************************************************************************************************************************************************************
+
+
+//*******************************************************************************************************************************************
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+    html {
+      font-family: Arial; 
+      display: inline-block; 
+      text-align: center;
+    }
+    h2 {
+      font-size: 3.0rem;
+    }
+    p {
+      font-size: 3.0rem;
+    }
+    body {
+      max-width: 600px; 
+      margin:0px auto; 
+      padding-bottom: 25px;
+    }
+    .switch {
+      position: relative; 
+      display: inline-block; 
+      width: 120px; 
+      height: 68px;
+    } 
+    .switch input {
+      display: none;
+    }
+    .slider {
+      position: absolute; 
+      top: 0; 
+      left: 0; 
+      right: 0; 
+      bottom: 0; 
+      background-color: #ccc; 
+      border-radius: 6px;
+    }
+    .slider:before {
+      position: absolute; 
+      content: ""; 
+      height: 52px; 
+      width: 52px; 
+      left: 8px; 
+      bottom: 8px; 
+      background-color: #fff; 
+      -webkit-transition: .4s; 
+      transition: .4s; 
+      border-radius: 3px
+    }
+    input:checked+.slider {
+      background-color: #b30000;
+    }
+    input:checked+.slider:before {
+      -webkit-transform: translateX(52px); 
+      -ms-transform: translateX(52px); 
+      transform: translateX(52px)
+    }
+  </style>
+</head>
+<body>
+  <h1>ESP Web Server</h1>
+  <p>
+    <span>Temperature</span> 
+    <span id="temp">%TEMPERATURE%</span>
+  </p>
+  <p>
+    <span>Humidity</span> 
+    <span id="humidity">%HUMIDITY%</span>
+  </p>
+  <p>
+    <span>Heat Index</span> 
+    <span id="heat">%HEAT%</span>
+  </p>
+  <script>
+    setInterval(function ( ) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("temp").innerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", "/temp", true);
+      xhttp.send();
+    }, 2000 ) ;
+
+    setInterval(function ( ) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("humidity").innerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", "/humidity", true);
+      xhttp.send();
+    }, 2000 ) ;
+
+    setInterval(function ( ) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("heat").innerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", "/heat", true);
+      xhttp.send();
+    }, 2000 ) ;
+
+    </script>
+</body>
+</html>)rawliteral";
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  if (var == "TEMPERATURE"){
+    return readTemp();
+  }
+  else if (var == "HUMIDITY"){
+    return readHumidity();
+  }
+  else if (var == "HEAT"){
+    return readHeat();
+  }
+  return String();
+}
+
+//*******************************************************************************************************************************************
+
+
+void setup() {
+  // Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+  delay(1500); 
+  pinMode(LED,OUTPUT);
+  dht.begin();
+  delay(1500);
+
+ WiFi.begin(SSID, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.println("Connected to WiFi");
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temp", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readTemp().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readHumidity().c_str());
+  });
+  server.on("/heat", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readHeat().c_str());
+  });
+
+  // Start server
+  server.begin();
+
+
+}
+
+void loop() {
+
+  delay(500);
+//
+//  humidity = dht.readHumidity();
+//  temperature = dht.readTemperature();
+//  // Read temperature as Fahrenheit (isFahrenheit = true)
+//  float f = dht.readTemperature(true);
+//
+//  // Check if any reads failed and exit early (to try again).
+//  if (isnan(humidity) || isnan(temperature) || isnan(f)) {
+//    Serial.println("Failed to read from DHT sensor!");
+//    return;
+//  }
+//
+//  // Compute heat index in Fahrenheit (the default)
+//  // float hif = dht.computeHeatIndex(f, h);
+//  // Compute heat index in Celsius (isFahreheit = false)
+//  heat = dht.computeHeatIndex(temperature, humidity, false);
 }
